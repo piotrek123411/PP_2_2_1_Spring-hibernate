726b940aa56d8e0a70e0b81577a858b0ed7818de
piotrek123411 <83031705+piotrek123411@users.noreply.github.com>
2022-10-03 10:26:31 +0300

    A

--- src/main/java/hiber/config/AppConfig.java
+++ src/main/java/hiber/config/AppConfig.java
@@ -1,5 +1,6 @@
 package hiber.config;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -41,11 +42,13 @@ public class AppConfig {
       factoryBean.setDataSource(getDataSource());
       
       Properties props=new Properties();
+      props.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
       props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
       props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
 
       factoryBean.setHibernateProperties(props);
-      factoryBean.setAnnotatedClasses(User.class);
+      factoryBean.setAnnotatedPackages();
+      factoryBean.setAnnotatedClasses(Car.class, User.class);
       return factoryBean;
    }
 
